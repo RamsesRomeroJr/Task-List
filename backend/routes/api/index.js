dc7a14ec8ff9366f1c8055f877d482f5085b85fb
ramses <ramses.romero.jr@gmail.com>
2021-03-23 19:27:21 -0400

    tested login/signup/logout auth

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,34 +1,10 @@
 const router = require('express').Router();
 const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-router.get('/set-token-cookie', asyncHandler(async (req, res) => {
-const user = await User.findOne({
-    where: {
-        username: 'Demo-lition'
-    },
-    })
-setTokenCookie(res, user);
-return res.json({ user });
-}));
+router.use('/session', sessionRouter);
 
-const { restoreUser } = require('../../utils/auth.js');
-router.get(
-  '/restore-user',
-  restoreUser,
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
-
-const { requireAuth } = require('../../utils/auth.js');
-router.get(
-  '/require-auth',
-  requireAuth,
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
+router.use('/users', usersRouter);
 
 module.exports = router;
